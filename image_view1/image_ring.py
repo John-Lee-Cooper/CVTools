e65cde67803c5cce35cd80a30d3210d49347cfb3
LeeCooper <lee.cooper@aciwebs.com>
2020-02-16 17:22:18 -0500

    Add typer

--- image_view1/image_ring.py
+++ image_view1/image_ring.py
@@ -19,26 +19,28 @@ class ImageRing:
         if len(image_paths_) == 1:
             first_image = image_paths_[0]
             image_paths_ = images_in_paths([first_image.parent])
+        if len(image_paths_) == 0:
+            exit()
         self._ring = RingBuffer(image_paths_, first_image)
         self._image_path = None
         self._image = None
-        self.fetch()
+        self._fetch()
 
     def __call__(self) -> Tuple[FilePath, Image]:
         return self._image_path, self._image.copy()
 
-    def fetch(self) -> None:
+    def _fetch(self) -> None:
         self._image_path = self._ring.value()
         self._image = imread(self._image_path)
 
     def next(self) -> None:
         self._ring.next()
-        self.fetch()
+        self._fetch()
 
     def prev(self) -> None:
         self._ring.prev()
-        self.fetch()
+        self._fetch()
 
     def pop(self) -> None:
         self._ring.pop()
-        self.fetch()
+        self._fetch()
